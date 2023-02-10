a9940df3afa881aa72d98b2b45c719c54e4c59a9
Usuario <Usuario@CPD02-M14>
2023-02-10 16:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ AppBuscarCep/AppBuscarCep/Model/Cidade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscarCep.Model
+{
+    internal class Cidade
+    {
+    }
+}
---
+++ AppBuscarCep/AppBuscarCep/Service/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscarCep.Service
+{
+    internal class DataService
+    {
+    }
+}
